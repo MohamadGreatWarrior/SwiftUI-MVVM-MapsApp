b6568e6a2fd649f90706f5c9b2160ee3d74f6593
Mohamad Tayan <mohamad.tn123@gmail.com>
2024-12-28 14:44:46 +0300

    Initial Commit

---
+++ MapsApp/MapsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsAppApp.swift
+//  MapsApp
+//
+//  Created by Mohamad Tayan on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
